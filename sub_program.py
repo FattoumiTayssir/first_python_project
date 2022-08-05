35c2b6571c0d0e3312301be5a4a1b165df725b35
Fattoumi Tayssir <fattoumitaycir@gmail.com>
2022-08-05 15:22:03 +0100

    docs(sub_program) : documenting adding function

--- sub_program.py
+++ sub_program.py
@@ -2,4 +2,13 @@ print('this is just a sub program')
 print("subprogram started")
 print("process 1")
 def adding_function(a,b):
+    """ this fucntion add two numbers
+
+    Args:
+        a (float): first float
+        b (float): second float
+
+    Returns:
+        flaot: the sum
+    """
     return a+b
